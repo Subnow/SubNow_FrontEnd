50fee2c5d54bab20dfe05f5362d769d4ceaf62db
Hesham1Gomaa <heshamgomaa01@gmail.com>
2024-05-17 20:30:36 +0300

    Adding Project File

---
+++ angular/src/environments/environment.prod.ts
@@ -0,0 +1,26 @@
+import { Environment } from '@abp/ng.core';
+
+const baseUrl = 'http://localhost:4200';
+
+export const environment = {
+  production: true,
+  application: {
+    baseUrl,
+    name: 'SubNow',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'https://localhost:44329/',
+    redirectUri: baseUrl,
+    clientId: 'SubNow_App',
+    responseType: 'code',
+    scope: 'offline_access SubNow',
+    requireHttps: true
+  },
+  apis: {
+    default: {
+      url: 'https://localhost:44329',
+      rootNamespace: 'SubNow',
+    },
+  },
+} as Environment;
